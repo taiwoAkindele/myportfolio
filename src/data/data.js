e0e1ae6fd854323bc95638b4cbf2339b15060f5b
Taiwo Akindele <akindeletaiwo214@gmail.com>
2023-12-13 16:42:17 +0100

    porfolio

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+import MOVIES from "../assets/projects/MOVIE.png";
+import CHATAI from "../assets/projects/CHAT-AI.png";
+import PIXEO from "../assets/projects/hero-pixeo.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: MOVIES,
+    github: "https://github.com/taiwoAkindele/Movie-Web-App",
+    live: "https://movie-web-app-one.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: CHATAI,
+    github: "https://github.com/taiwoAkindele/CHAT-AI-frontend",
+    live: "https://chat-ai-frontend-one.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: PIXEO,
+    github: "https://github.com/taiwoAkindele/pixeo",
+    live: "https://poetic-fox-0d8c7d.netlify.app/",
+  },
+  // {
+  //     id:4,
+  //     name:"React JS Application",
+  //     image:RealEst,
+  //     github:"",
+  //     live:"",
+  // },
+];
